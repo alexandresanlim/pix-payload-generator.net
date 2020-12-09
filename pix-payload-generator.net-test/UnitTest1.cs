421b04362b2845b9d27caf76e5dc1e8f0195a004
Alexandre Lima <alexandreslima@outlook.com>
2020-12-09 12:04:40 -0300

    rename

--- pix-payload-generator.net-test/UnitTest1.cs
+++ pix-payload-generator.net-test/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace pix_payload_generator.net_test
     public class UnitTest1
     {
         [TestMethod]
-        public void CreatePayload()
+        public void CreatePayloadStatic()
         {
             var payload = new Payload("bee05743-4291-4f3c-9259-595df1307ba1", 10.00m, "Um-Id-Qualquer", new Merchant("Alexandre Lima", "Presidente Prudente"));
 
