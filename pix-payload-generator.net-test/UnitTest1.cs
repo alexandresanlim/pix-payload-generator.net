8620b88f11d18c620f4358505424bf5a31af53e3
Alexandre Lima <alexandreslima@outlook.com>
2020-12-09 11:42:12 -0300

    add logo

--- pix-payload-generator.net-test/UnitTest1.cs
+++ pix-payload-generator.net-test/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace pix_payload_generator.net_test
         [TestMethod]
         public void CreatePayload()
         {
-            var payload = new Payload("41703107802", 00.50m, "Um-Id-Qualquer", new Merchant("Alexandre Lima", "Presidente Prudente"));
+            var payload = new Payload("bee05743-4291-4f3c-9259-595df1307ba1", 10.00m, "Um-Id-Qualquer", new Merchant("Alexandre Lima", "Presidente Prudente"));
 
             var stringToQrCode = payload.Generate();
 
